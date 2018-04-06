bba906df141cd857b9e5a7a204dce41112c7414c
Chahat Goel <chahatgoel98@gmail.com>
2018-04-06 19:32:32 +0530

    learning basics

---
+++ game.py
@@ -0,0 +1,15 @@
+import random, time, sys
+
+user_score = 0
+comp_score = 0
+comp_gain = 0
+user_gain = 0
+track = 0
+play_again ='Y'
+list = [ "stone", "paper", "scissor"]
+
+
+print("Welcome to the stone paper scissor game")
+print("So let's start")
+
+while play_again == 'Y' or play_again == 'y' :
